ff8bb7b2695992417c0c2fc2d2b5549b8e5ccd7f
Aryan :) <94476184+Aryboy240@users.noreply.github.com>
2024-11-12 22:24:43 +0000

    Laravel Overhall
    I got a lot wrong in the old implementation, so I restarted. Better to do that earlier rather than finding out much later

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/login', function () {
+    return view('login'); // Refers to resources/views/login.blade.php
+})->name('login');
+
+
+Route::get('/register', function () {
+    return view('register'); // Refers to resources/views/register.blade.php
+})->name('register');
+
