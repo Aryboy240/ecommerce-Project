f51b1c948be0e2f943b8d6bb5ca5f0be330f76ae
Aryan :) <94476184+Aryboy240@users.noreply.github.com>
2024-12-04 21:49:14 +0000

    Aryan's css fix

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!--CSS-->
   <link rel="stylesheet" href="{{ asset('css/login.css') }}"> 
-  <link rel="stylesheet" href="{{ asset('css/aryansExtras.css') }}"> 
+  <link rel="stylesheet" href="css/aryansExtras.css"> 
   <!--Fonts-->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
