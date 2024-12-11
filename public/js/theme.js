92be75af52ad26a275c55d39b84304c1fff12720
Aronveer Shergill <230416624@aston.ac.uk>
2024-12-11 08:49:22 +0000

    Zoom improvements

--- public/js/theme.js
+++ public/js/theme.js
@@ -25,29 +25,37 @@ function toggleTheme() {
 //Zoom Feature
 
 document.querySelectorAll('.product-card img').forEach(img => {
-    img.addEventListener('mouseover', () => {
-        img.style.cursor = 'zoom-in';
+    let isZoomed = false;
+
+    img.addEventListener('mousemove', event => {
+        if (isZoomed) {
+            const rect = img.getBoundingClientRect();
+            const offsetX = event.clientX - rect.left; 
+            const offsetY = event.clientY - rect.top; 
+            const width = rect.width;
+            const height = rect.height;
+
+            const originX = (offsetX / width) * 100;
+            const originY = (offsetY / height) * 100;
+
+            img.style.transformOrigin = `${originX}% ${originY}%`;
+        }
     });
 
-    img.addEventListener('click', event => {
-        const rect = img.getBoundingClientRect(); 
-        const offsetX = event.clientX - rect.left; 
-        const offsetY = event.clientY - rect.top; 
-        const width = rect.width; 
-        const height = rect.height;
-        if (img.style.transform === 'scale(2)') {
+    img.addEventListener('click', () => {
+        if (isZoomed) {
             img.style.transform = 'scale(1)';
-            img.style.transformOrigin = 'center center'; 
+            img.style.transformOrigin = 'center center';
             img.style.cursor = 'zoom-in';
+            isZoomed = false;
         } else {
-            const originX = (offsetX / width) * 100; 
-            const originY = (offsetY / height) * 100;
-            img.style.transformOrigin = `${originX}% ${originY}%`; 
-            img.style.transform = 'scale(2)'; 
+            img.style.transform = 'scale(2)';
             img.style.transition = 'transform 0.3s ease';
             img.style.cursor = 'zoom-out';
+            isZoomed = true;
         }
     });
 });
+
 
 document.getElementById("themeButton").onclick = toggleTheme;
